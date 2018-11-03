1a3897e7eb9a31deb7df1fe7cc8d206ba83137e1
DaringATK <daring.atk@gmail.com>
2018-11-04 01:35:39 +0300

    homework 7

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -1,29 +1,22 @@
-# Provider google
-
 provider "google" {
   version = "1.4.0"
+
   project = "${var.project}"
   region  = "${var.region}"
 }
 
 module "app" {
-  source             = "../modules/app"
-  public_key_path    = "${var.public_key_path}"
-  zone               = "${var.zone}"
-  app_disk_image     = "${var.app_disk_image}"
-  private_key_path   = "${var.private_key_path}"
-  provision_var      = "${var.provision_var}"
-  files_deploy_sh    = "${var.files_deploy_sh}"
-  files_puma_service = "${var.files_puma_service}"
+  source          = "../modules/app"
+  public_key_path = "${var.public_key_path}"
+  zone            = "${var.zone}"
+  app_disk_image  = "${var.app_disk_image}"
 }
 
 module "db" {
-  source           = "../modules/db"
-  public_key_path  = "${var.public_key_path}"
-  zone             = "${var.zone}"
-  db_disk_image    = "${var.db_disk_image}"
-  private_key_path = "${var.private_key_path}"
-  provision_var    = "${var.provision_var}"
+  source          = "../modules/db"
+  public_key_path = "${var.public_key_path}"
+  zone            = "${var.zone}"
+  db_disk_image   = "${var.db_disk_image}"
 }
 
 module "vpc" {
