9653c00a0780a1c3be2ef58736f3ae4a9ab15346
DaringATK <daring.atk@gmail.com>
2018-11-04 01:20:33 +0300

    homework 7

--- terraform/prod/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket = "storage-bucket-db"
-    prefix = "terraform/state"
-  }
-}
--- terraform/prod/output.tf
+++ terraform/prod/output.tf
@@ -2,6 +2,3 @@ output "app_external_ip" {
   value = "${module.app.app_external_ip}"
 }
 
-output "db_external_ip" {
-  value = "${module.db.db_external_ip}"
-}
